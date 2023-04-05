b20b4db52617bc067ff9322a22b741cd3ca99f55
RobertYoung2022 <robertayoung2022@gmail.com>
2023-04-05 02:46:09 -0500

    created numbers file & completed exercises

---
+++ Exercises/Numbers.py
@@ -0,0 +1,12 @@
+# 1. What is your age? Write a series of calculations that result in your age being printed to the screen.
+myAge = 34
+print(myAge)
+# 2. Think of a number that never changes. Create a constant to represent that number and print it to the screen.
+SEC_IN_MIN = 60
+print(SEC_IN_MIN)
+# NOTE: Constants = Use all capital letters. Constant is a variable with a value that never changes
+
+# (BONUS) Assign values to variables in one line
+heNum, sheNum, itNum =  6, 15, 31
+print(heNum, sheNum, itNum)
+print(heNum)
